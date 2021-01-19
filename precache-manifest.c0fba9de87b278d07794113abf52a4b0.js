a59b764a1a78e12210c33c8c4f3a1442908d2543
Andrii Haidai <haidai.andriy@gmail.com>
2021-01-19 11:56:07 +0200

    Updates

---
+++ precache-manifest.c0fba9de87b278d07794113abf52a4b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73d9d31cc881f0c08c69f14b1fb04e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f786cce3efe547d2495b",
+    "url": "./static/css/main.f64a0711.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f786cce3efe547d2495b",
+    "url": "./static/js/main.f51c22d7.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
